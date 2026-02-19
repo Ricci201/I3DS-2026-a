4941d2b839da6c4ce37bdba103b558d54409b96f
Ricci201 <lucas.ricci@aluno.senai.br>
2026-02-19 11:18:37 -0300

    feat: adiciona componente Rodape e implementa a estrutura do App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming Devflix, com letras vermelhas e fundo preto, promovendo conteúdo de séries, filmes e entretenimento online."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquisa por filmes e séries..." />
+        <img role="button" src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+      <Rodape>Lucas Ricci</Rodape>
+    </div>
+  );
+};
+
+export default App;
