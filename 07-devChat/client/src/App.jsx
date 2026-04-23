8c895ab4f6f7b1b5b6fb2542b4c1a75ef2a987a8
Ricci 201 <lucas.ricci@aluno.senai.br>
2026-04-23 16:32:26 -0300

    Add chat component with message handling and styling
    - Implemented Chat component to handle real-time messaging using socket.io.
    - Added message input and send functionality with Enter key support.
    - Created Chat.module.css for styling the chat interface, including message display and layout.
    - Integrated message reception and display logic to update the chat dynamically.

--- 07-devChat/client/src/App.jsx
+++ 07-devChat/client/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 function App() {
+  const [chatVisible, setChatVisible] = useState(false);
+  const [socket, setSocket] = useState(null);
   return (
     <>
-      <Join />
+      {chatVisible ? (
+        <Chat socket={socket} />
+      ) : (
+        <Join setSocket={setSocket} setChatVisible={setChatVisible} />
+      )}
     </>
   );
-}
+} 
 
 export default App;
